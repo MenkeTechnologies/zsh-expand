c8b5b16ce766623b57c82a5bdbd585d0a470a460
MenkeTechnologies <linux.dev25@gmail.com>
2020-03-28 23:03:16 -0400

    do not expand noglob aliases

--- zsh-expand.plugin.zsh
+++ zsh-expand.plugin.zsh
@@ -1,6 +1,6 @@
 #{{{                    MARK:variables
 #**************************************************************
-blacklistFirstPosRegex='=.?(grc|_z|zshz|cd|hub|_zsh_tmux_.*|_rails_.*|_rake_.*|mvn-or.*|gradle-or.*)'
+blacklistFirstPosRegex='=.?(grc|_z|zshz|cd|hub|_zsh_tmux_.*|_rails_.*|_rake_.*|mvn-or.*|gradle-or.*|noglob .*)'
 continueFirstPositionRegex='\b(sudo|zpwr|env)\b'
 continueSecondPositionRegex='^(\-.*|env)$'
 
