ad20c0023db69f4183eb5d0b5f428324efff4e85
MenkeTechnologies <linux.dev25@gmail.com>
2020-08-16 23:48:37 -0400

    set ZERO environment var

--- zsh-expand.plugin.zsh
+++ zsh-expand.plugin.zsh
@@ -13,6 +13,7 @@
 #
 # According to the standard:
 # http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+ZERO="${(%):-%N}" # this gives immunity to functionargzero being unset
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
