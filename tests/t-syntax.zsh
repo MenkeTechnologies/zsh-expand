accf2adbcd7eec615364124947612e8a14f9b796
MenkeTechnologies <linux.dev25@gmail.com>
2020-10-01 15:50:17 -0400

    add basic tests

---
+++ tests/t-syntax.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zunit
+#{{{                    MARK:Header
+#**************************************************************
+##### Author: MenkeTechnologies
+##### GitHub: https://github.com/MenkeTechnologies
+##### Date: Tue Feb 25 19:37:50 EST 2020
+##### Purpose: zsh script to
+##### Notes:
+#}}}***********************************************************
+
+@setup {
+    0="${${0:#$ZSH_ARGZERO}:-${(%):-%N}}"
+    0="${${(M)0:#/*}:-$PWD/$0}"
+    pluginDir="${0:h:A}"
+}
+
+@test 'plugin *.zsh' {
+	for file in "$pluginDir/"*.zsh; do
+        run zsh -n "$file"
+        assert $state equals 0
+    done
+}
